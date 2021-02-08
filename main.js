941c7b44ffdc488e0025baeac3d432cde46a3f27
AnuSra <76482056+AnuSra@users.noreply.github.com>
2021-02-08 14:46:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Canvas=document.getElementById("myCanvas");
+ctx=canvas.getContex("2d");
+
+
+car1_width=120;
+car1_height=70;
+car1_image="car1.png";
+car1_x=10;
+car1_y=10;
+
+car2_width=120;
+car2_height=70;
+car2_image="car2.png";
+car2_x=10;
+car2_y=100;
